3bdf64cc03a88401334300fb822f4aa03f006f2e
Motiur Muhith <1motiur4@gmail.com>
2022-07-25 23:19:55 -0400

    Used functions to reduce lines. Movies are displayed with one function for both main and favorites.

--- app.js
+++ app.js
@@ -1,171 +1,109 @@
-const apiUrl =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=025578d5ab67135f221a9d7ce8559986&page=1";
+const apiKey = "025578d5ab67135f221a9d7ce8559986";
+const MAINURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+  apiKey +
+  "&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?&api_key=025578d5ab67135f221a9d7ce8559986&query=";
+const SEARCHURL =
+  "https://api.themoviedb.org/3/search/movie?&api_key=" + apiKey + "&query=";
 
-function searchById(movie_ID) {
-  const url =
-    "https://api.themoviedb.org/3/movie/" +
-    movie_ID +
-    "?&api_key=025578d5ab67135f221a9d7ce8559986";
+var onFav = false;
+
+// This grabs a movie by its ID -- for Favorites page
+function showFavorites() {
+  onFav = true;
+  main.innerHTML = "";
+  function favURL(movie_ID) {
+    const url =
+      "https://api.themoviedb.org/3/movie/" + movie_ID + "?&api_key=" + apiKey;
+    return url;
+  }
+
+  favoritesArray.forEach((element) => {
+    fetch(favURL(element))
+      .then((res) => res.json())
+      .then(function (data) {
+        createPage(data);
+      });
+  });
+}
+
+// This grabs an array of movies
+function getMoviesData(url) {
+  onFav = false;
+  main.innerHTML = "";
   fetch(url)
     .then((res) => res.json())
     .then(function (data) {
-      return data.title;
+      data.results.forEach((element) => {
+        createPage(element);
+      });
     });
 }
 
-var favArray = [];
+// Array for storing favorites, which will get converted into a string to store in local storage
+favoritesArray = [];
 if (localStorage.getItem("favorites") == null) {
-  localStorage.setItem("favorites", JSON.stringify(favArray));
+  localStorage.setItem("favorites", JSON.stringify(favoritesArray));
 } else {
-  favArray = JSON.parse(localStorage.getItem("favorites"));
+  favoritesArray = JSON.parse(localStorage.getItem("favorites"));
 }
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-function showMovies(url) {
-  main.innerHTML = "";
-  fetch(url)
-    .then((res) => res.json())
-    .then(function (data) {
-      data.results.forEach((element) => {
-        const image = document.createElement("img");
-        const movieTitle = document.createElement("h2");
+function createPage(movieData) {
+  const image = document.createElement("img");
+  const movieTitle = document.createElement("h2");
 
-        movieTitle.innerHTML = `${element.title}`;
-        image.src = IMGPATH + element.poster_path;
+  movieTitle.innerHTML = `${movieData.title}`;
+  image.src = IMGPATH + movieData.poster_path;
 
-        const containerDiv = document.createElement("div");
-        containerDiv.className = "container";
-        containerDiv.appendChild(image);
+  const containerDiv = document.createElement("div");
+  containerDiv.className = "container";
+  containerDiv.appendChild(image);
 
-        const overlayDiv = document.createElement("div");
-        overlayDiv.className = "overlay";
-        overlayDiv.appendChild(movieTitle);
-        containerDiv.appendChild(overlayDiv);
+  const overlayDiv = document.createElement("div");
+  overlayDiv.className = "overlay";
+  overlayDiv.appendChild(movieTitle);
+  containerDiv.appendChild(overlayDiv);
 
-        const addToFav = document.createElement("button");
-        addToFav.className = "fav";
+  const addToFav = document.createElement("button");
+  addToFav.className = "fav";
 
-        if (favArray.includes(element.id.toString())) {
-          addToFav.innerText = "Remove";
-          addToFav.onclick = removeFromFavF;
-        } else {
-          addToFav.innerText = "Favorite";
-          addToFav.onclick = addToFavF;
-        }
-        containerDiv.appendChild(addToFav);
-
-        main.appendChild(containerDiv);
-
-        var movieId = `${element.id}`;
-        function addToFavF() {
-          favArray.push(movieId);
-          localStorage.setItem("favorites", JSON.stringify(favArray));
-          showAlert(`${element.title} has been added to your favorites!`);
-          addToFav.innerText = "Remove";
-          addToFav.onclick = removeFromFavF;
-        }
-        function removeFromFavF() {
-          var idToRemove = favArray.findIndex(checkID);
-          favArray.splice(idToRemove, 1);
-          localStorage.setItem("favorites", JSON.stringify(favArray));
-          showAlert(`${element.title} has been removed from your favorites!`);
-          addToFav.innerText = "Favorite";
-          addToFav.onclick = addToFavF;
-        }
-        function checkID(ID) {
-          return ID == movieId;
-        }
-      });
-    });
-}
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  main.innerHTML = "";
-  const searchTerm = search.value;
-
-  if (searchTerm) {
-    showMovies(SEARCHAPI + searchTerm);
-    search.value = "";
-  }
-});
-
-showMovies(apiUrl);
-
-function favURL(movie_ID) {
-  const url =
-    "https://api.themoviedb.org/3/movie/" +
-    movie_ID +
-    "?&api_key=025578d5ab67135f221a9d7ce8559986";
-  return url;
-}
-
-function showFavorites() {
-  if (localStorage.getItem("favorites") == null) {
-    alert("You have no favorites!");
+  if (favoritesArray.includes(movieData.id.toString())) {
+    addToFav.innerText = "Remove";
+    addToFav.onclick = removeFromFavF;
   } else {
-    main.innerHTML = "";
-    favArray.forEach((element) => {
-      fetch(favURL(element))
-        .then((res) => res.json())
-        .then(function (data) {
-          const image = document.createElement("img");
-          const movieTitle = document.createElement("h2");
+    addToFav.innerText = "Favorite";
+    addToFav.onclick = addToFavF;
+  }
 
-          movieTitle.innerHTML = `${data.title}`;
-          image.src = IMGPATH + data.poster_path;
+  containerDiv.appendChild(addToFav);
+  main.appendChild(containerDiv);
 
-          const containerDiv = document.createElement("div");
-          containerDiv.className = "container";
-          containerDiv.appendChild(image);
-
-          const overlayDiv = document.createElement("div");
-          overlayDiv.className = "overlay";
-          overlayDiv.appendChild(movieTitle);
-          containerDiv.appendChild(overlayDiv);
-
-          const addToFav = document.createElement("button");
-          addToFav.className = "fav";
-
-          if (favArray.includes(data.id.toString())) {
-            addToFav.innerText = "Remove";
-            addToFav.onclick = removeFromFavF;
-          } else {
-            addToFav.innerText = "Favorite";
-            addToFav.onclick = addToFavF;
-          }
-          containerDiv.appendChild(addToFav);
-
-          main.appendChild(containerDiv);
-
-          var movieId = `${data.id}`;
-          function addToFavF() {
-            favArray.push(movieId);
-            localStorage.setItem("favorites", JSON.stringify(favArray));
-            showAlert(`${data.title} has been added to your favorites!`);
-            addToFav.innerText = "Remove";
-            addToFav.onclick = removeFromFavF;
-          }
-          function removeFromFavF() {
-            var idToRemove = favArray.findIndex(checkID);
-            favArray.splice(idToRemove, 1);
-            localStorage.setItem("favorites", JSON.stringify(favArray));
-            showAlert(`${data.title} has been removed from your favorites!`);
-            addToFav.innerText = "Favorite";
-            addToFav.onclick = addToFavF;
-            showFavorites();
-          }
-          function checkID(ID) {
-            return ID == movieId;
-          }
-        });
-    });
+  var movieId = `${movieData.id}`;
+  function addToFavF() {
+    favoritesArray.push(movieId);
+    localStorage.setItem("favorites", JSON.stringify(favoritesArray));
+    showAlert(`${movieData.title} has been added to your favorites!`);
+    addToFav.innerText = "Remove";
+    addToFav.onclick = removeFromFavF;
+  }
+  function removeFromFavF() {
+    var idToRemove = favoritesArray.findIndex(checkID);
+    favoritesArray.splice(idToRemove, 1);
+    localStorage.setItem("favorites", JSON.stringify(favoritesArray));
+    showAlert(`${movieData.title} has been removed from your favorites!`);
+    addToFav.innerText = "Favorite";
+    addToFav.onclick = addToFavF;
+    if (onFav) {
+      showFavorites();
+    }
+  }
+  function checkID(ID) {
+    return ID == movieId;
   }
 }
 
@@ -177,3 +115,5 @@ function showAlert(message) {
     }, 4000);
   });
 }
+
+getMoviesData(MAINURL);
